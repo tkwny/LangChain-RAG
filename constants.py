59dc771f8f3f02adc64cd9467333fe9fd248abb7
Randy Bradshaw <randy.bradshaw@gmail.com>
2024-12-10 13:41:19 -0600

    first commit

---
+++ constants.py
@@ -0,0 +1,9 @@
+# Constants for color codes
+RESET = "\033[0m"  # Resets all colors and styles
+RED = "\033[31m"
+GREEN = "\033[32m"
+YELLOW = "\033[33m"
+BLUE = "\033[34m"
+MAGENTA = "\033[35m"
+CYAN = "\033[36m"
+
